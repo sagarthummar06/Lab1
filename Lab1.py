72e6090736c683e2b9b2f5830f89ef2c860c4fdb
sagarthummar06 <sagarthummar6@gmail.com>
2023-05-26 17:00:44 -0400

    First commit

---
+++ Lab1.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
+# Example usage
+num1 = 10
+num2 = 5
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
